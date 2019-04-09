a56e5d06ab9c74a05609d8f873730893fa519e46
ageinpee <henrik.peters.98@web.de>
2019-04-09 13:27:39 +0200

    started run function, but problems with import

--- IGMO_Sim1/main.py
+++ IGMO_Sim1/main.py
@@ -1,4 +1,14 @@
 import uuid
+from .LKW import LKW
+from .LKW import LKW_state_enum
 
 def run(rounds, no_LKW, no_loadingstations, no_scales):
+    list_of_LKWs = []
 
+    for lkw in no_LKW:
+        list_of_LKWs.append(LKW(uuid.uuid1(), LKW_state_enum.LOADING_QUEUE, 0))
+
+    print(list_of_LKWs)
+
+
+run(0, 5, 0, 0)
