8b15819993cb4bb45e8be77d32bda74a585a0389
ageinpee <henrik.peters.98@web.de>
2019-04-09 13:04:48 +0200

    LKW class seems good so far. more modeling has to be made though

--- IGMO_Sim1/main.py
+++ IGMO_Sim1/main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import uuid
+import random
 
 
 class LKW_state_enum(Enum):
@@ -16,7 +17,6 @@ next_state = {LKW_state_enum.LOADING: LKW_state_enum.WEIGHTING_QUEUE,
                   LKW_state_enum.EMPTYING: LKW_state_enum.LOADING_QUEUE}
 
 
-
 class LKW:
 
     def __init__(self, id, state, waiting_time):
@@ -26,6 +26,37 @@ class LKW:
 
         next_event_at = 0
         next_event_type = next_state[self.state]
+
+    def next_state(self):
+        self.state = next_state[self.state]
+        self.waiting_time = self.update_waiting_time(self.state)
+
+    def update_waiting_time(self, state):
+        rand = random.randomrange(0, 100)
+        print(rand)
+        if state == LKW_state_enum.LOADING:
+            if rand < 10:
+                return 16.0
+            elif rand < 30:
+                return 10.0
+            elif rand > 30:
+                return 12.0
+        elif state == LKW_state_enum.WEIGHTING:
+            if rand < 20:
+                return 24.0
+            elif rand < 35:
+                return 18.0
+            elif rand > 35:
+                return 16.0
+        elif state == LKW_state_enum.EMPTYING:
+            if rand < 10:
+                return 120
+            elif rand < 15:
+                return 80
+            elif rand > 15:
+                return 60
+        else:
+            return 0
 
 
 def run(rounds, no_LKW, no_loadingstations, no_scales):
